1eb8de2ffdd823198e8979712b82a97e32e2efd6
harry13246577 <harryeastwood5@gmail.com>
2024-12-17 02:43:31 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// FAQ Accordion Logic
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling;
+
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+    });
+});
